ba6368dfb592512452f9a1641e5e5abce741952f
farazali-110 <151817304+farazali-110@users.noreply.github.com>
2024-02-12 20:06:05 +0500

    Add files via upload

---
+++ logical1.py
@@ -0,0 +1,23 @@
+number=int(input("enter a number:"))
+if number ==0:
+    print("given number is zero:")
+elif number<0:
+    print(number,"is a negative number:")
+else:
+    print(number,"is a positive number:")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
